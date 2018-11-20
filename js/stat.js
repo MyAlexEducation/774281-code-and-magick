e2a821f75451bed42f52e6cb67c135461bcf0f1a
Alex <MyAlexEducation@gmail.com>
2018-11-20 09:35:44 +0300

    добавляет функцию отрисовки облака

--- js/stat.js
+++ js/stat.js
@@ -1,3 +1,8 @@
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
 window.renderStatistics = function (ctx, players, times) {
-  
+
 }
