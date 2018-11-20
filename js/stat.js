2a801f67e862a425856006fde6869ecadc770a7c
Alex <MyAlexEducation@gmail.com>
2018-11-20 11:18:12 +0300

    задаёт цвета для облака и тени как константы и подставляет их в вызов для отрисовки

--- js/stat.js
+++ js/stat.js
@@ -4,6 +4,8 @@ var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var SHADOW_SHIFT_X = 10;
 var SHADOW_SHIFT_Y = 10;
+var CLOUD_COLOR = '#fff';
+var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 
 var renderCloud = function(ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -11,6 +13,6 @@ var renderCloud = function(ctx, x, y, color) {
 };
 
 window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + SHADOW_SHIFT_X, CLOUD_Y + SHADOW_SHIFT_Y, 'rgba(0, 0, 0, 0.3)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  renderCloud(ctx, CLOUD_X + SHADOW_SHIFT_X, CLOUD_Y + SHADOW_SHIFT_Y, SHADOW_COLOR);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 };
