3187f40743ae1d919266a99d0d9778f5fe279357
Alex <MyAlexEducation@gmail.com>
2018-11-20 11:48:11 +0300

    задаёт константы для отступов от краёв облака и между столбцами

--- js/stat.js
+++ js/stat.js
@@ -6,8 +6,11 @@ var SHADOW_SHIFT_X = 10;
 var SHADOW_SHIFT_Y = 10;
 var CLOUD_COLOR = '#fff';
 var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
+var MESSAGE_FONT = '16px "PT Mono"';
+var MARGIN_COLUMN = 50;
+var PADDING_CLOUD = MARGIN_COLUMN / 2;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
@@ -15,4 +18,8 @@ var renderCloud = function(ctx, x, y, color) {
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + SHADOW_SHIFT_X, CLOUD_Y + SHADOW_SHIFT_Y, SHADOW_COLOR);
   renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
+
+  ctx.font = MESSAGE_FONT;
+  ctx.strokeText();
+  ctx.strokeText();
 };
